c0dc12cdc98c59b93e80b7bf1195c96d9cd2e036
Gülşah Genç <gulsahgenc.gg@gmail.com>
2021-04-29 21:33:43 +0300

    Changed styletronfile

--- helpers/styletron.js
+++ helpers/styletron.js
@@ -1,18 +1,11 @@
-/*
-Copyright (c) 2018-2020 Uber Technologies, Inc.
-This source code is licensed under the MIT license found in the
-LICENSE file in the root directory of this source tree.
-*/
+import { Client, Server } from 'styletron-engine-atomic'
 
-/* eslint-disable flowtype/require-valid-file-annotation */
-/* global document */
+const getHydrateClass = () =>
+  document.getElementsByClassName('_styletron_hydrate_')
 
-import {Client, Server} from 'styletron-engine-atomic';
-
-export const isServer = typeof window === 'undefined';
-
-const getHydrate = () => document.getElementsByClassName('_styletron_hydrate_');
-
-export const styletron = isServer
+export const styletron =
+  typeof window === 'undefined'
     ? new Server()
-    : new Client({hydrate: getHydrate()});
+    : new Client({
+        hydrate: getHydrateClass(),
+      })
