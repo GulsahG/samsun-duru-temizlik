3602fd8dab551be76ca2a1de5450a74812850261
Gülşah Genç <gulsahgenc.gg@gmail.com>
2021-07-12 01:39:58 +0300

    Next-seo moved to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/fonts.sass';
 import '../styles/style.scss';
 import Nav from '../src/components/Nav';
+import { NextSeo } from 'next-seo';
 import  { BreakpointProvider } from 'react-socks';
 import { ThemeProvider, StyleReset } from 'atomize';
 import { Provider as StyletronProvider } from "styletron-react";
@@ -53,6 +54,31 @@ const MyApp = ({ Component, pageProps }) => {
         <BreakpointProvider>
           <script>0</script>
           <StyleReset />
+          <NextSeo
+            title="Samsun Duru Temizlik"
+            description="Samsun Duru Temizlik Samsun'un her yerinde ev, ofis temizliği servisi veriyor."
+            canonical={process.env.PAGE_URL}
+            mobileAlternate={{
+              media: 'only screen and (max-width: 640px)',
+              href: process.env.MOBILE_PAGE_URL,
+            }}
+            openGraph={{
+              type: 'website',
+              locale: 'tr_TR',
+              url: process.env.PAGE_URL,
+              title: 'Samsun Duru Temizlik Şirketi',
+              description: 'Samsun Duru Temizlik Samsun\'un her yerinde ev, ofis temizliği servisi veriyor.',
+              images: [
+                {
+                  url: 'https://samsun-duru-temizlik.vercel.app/_next/static/images/logo-220a15d9f60b5967a6d7e50b3c4b8e34.svg',
+                  width: 548,
+                  height: 372,
+                  alt: 'Şirket logosu-duru temizlik',
+                }
+              ],
+              site_name: 'Duru Temizlik',
+            }}
+          />
           <Nav />
           <Component {...pageProps} />
         </BreakpointProvider>
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,9 @@
 import React from 'react';
-import { NextSeo } from 'next-seo';
 import Homepage from './Homepage';
 
 const IndexPage = () => {
   return (
-    <div>
-      <NextSeo
-        title="Samsun Duru Temizlik"
-        description="Samsun Duru Temizlik Samsun'un her yerinde ev, ofis temizliği servisi veriyor."
-        canonical={process.env.PAGE_URL}
-        mobileAlternate={{
-          media: 'only screen and (max-width: 640px)',
-          href: process.env.MOBILE_PAGE_URL,
-        }}
-        openGraph={{
-          type: 'website',
-          locale: 'tr_TR',
-          url: process.env.PAGE_URL,
-          title: 'Samsun Duru Temizlik Şirketi',
-          description: 'Samsun Duru Temizlik Samsun\'un her yerinde ev, ofis temizliği servisi veriyor.',
-          images: [
-            {
-              url: 'https://samsun-duru-temizlik.vercel.app/_next/static/images/logo-220a15d9f60b5967a6d7e50b3c4b8e34.svg',
-              width: 548,
-              height: 372,
-              alt: 'Şirket logosu-duru temizlik',
-            }
-          ],
-          site_name: 'Duru Temizlik',
-        }}
-      />
       <Homepage />
-    </div>
   );
 };
 
